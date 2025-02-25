18ff88d7523eb96733d2bd533461ec34a3f85bbc
Ross Smith <smithross2003@gmail.com>
2025-02-25 22:45:16 +0000

    Fix parallel

--- projects/test/src/init.lua
+++ projects/test/src/init.lua
@@ -1,11 +1,11 @@
-package.path = package.path..";../../../../src/?.lua"
+package.path = package.path..";../../../src/?.lua"
 local obelisk = require("Obelisk")
 local service = obelisk.m("service")
 
 require("services/TestService")
 
 obelisk.init()
-service.withParallel(function()
+obelisk.withParallel(function()
     while true do
         os.sleep(0)
     end
