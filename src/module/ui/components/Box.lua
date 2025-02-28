84ccfa8bbe00519cb2d8eb2d785158fdb3d19358
Ross Smith <smithross2003@gmail.com>
2025-02-28 21:50:06 +0000

    fix: Attempt fix render

--- src/module/ui/components/Box.lua
+++ src/module/ui/components/Box.lua
@@ -1,6 +1,7 @@
 local Component = require("module/ui/components/Component")
 
 local Box = Component:extend(function(self)
+    print("RENDER!")
     local position, size, color = self.props.position, self.props.size, self.props.color
     paintutils.drawFilledBox(position.x, position.y, size.x, size.y, color)
 end)
