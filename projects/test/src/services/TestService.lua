8c6dd1961b5389f373ecd1cb21f203c66eadcef9
Ross Smith <smithross2003@gmail.com>
2025-03-01 03:15:29 +0000

    refactor: Update app definition

--- projects/test/src/services/TestService.lua
+++ projects/test/src/services/TestService.lua
@@ -17,24 +17,7 @@ service.registerService("UIService", {
         end
         term.setBackgroundColor(colors.black)
         term.setCursorPos(0,0)
-        ui.App("app", {
-            TestComponent:withProps("Test1", {
-                id = "Test1",
-                position = {x = 2, y = 2},
-                size = {x = 2, y = 2},
-            })(),
-            TestComponent:withProps("Test2", {
-                id = "Test2",
-                position = {x = 10, y = 10},
-                size = {x = 2, y = 2},
-                color = colors.red,
-            })(),
-            TestComponent:withProps("Test3", {
-                id = "Test3",
-                position = {x = 5, y = 5},
-                size = {x = 2, y = 2},
-            })(),
-        })
+        ui.App("testApp")
         term.setBackgroundColor(colors.black)
     end
 })
--- src/module/ui/example/App.lua
+++ src/module/ui/example/App.lua
@@ -1,4 +1,23 @@
 local Component = require("module/ui/components/Component")
 local TestComponent = require("module/ui/example/TestComponent")
 
-return Component:new()
+return function(id) 
+    return Component:new()(id or "app", {
+        TestComponent:withProps("Test1", {
+            id = "Test1",
+            position = {x = 2, y = 2},
+            size = {x = 2, y = 2},
+        })(),
+        TestComponent:withProps("Test2", {
+            id = "Test2",
+            position = {x = 10, y = 10},
+            size = {x = 2, y = 2},
+            color = colors.red,
+        })(),
+        TestComponent:withProps("Test3", {
+            id = "Test3",
+            position = {x = 5, y = 5},
+            size = {x = 2, y = 2},
+        })(),
+    })
+end
